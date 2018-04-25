2a6c5ca4b4b5ad5fabf925ea0d1e616d0775c785
Chad Woolley <thewoolleyman@gmail.com>
2018-04-25 16:02:01 -0700

    fixes to pipeline.sh

--- nonapp/cicd/bk/pipeline.sh
+++ nonapp/cicd/bk/pipeline.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 set -eu
 
-nonapp/cicd/bk/generate-pipeline-yml /tmp/monorepo-pipeline.yml
-cat /tmp/monorepo-pipeline.yml | buildkite-agent pipeline upload
+pipeline_file=/tmp/monorepo-pipeline.yml
+$(dirname "${BASH_SOURCE}")/generate-pipeline-yml ${pipeline_file}
+
+if [[ -e ${pipeline_file} ]]
+then
+  cat ${pipeline_file} | buildkite-agent pipeline upload
+else
+  echo "${pipeline_file} was not generated, not triggering any app builds."
+fi
+
