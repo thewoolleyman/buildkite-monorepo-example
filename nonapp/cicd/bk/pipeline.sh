1c3f2aa679626a64664ddf3e50fafebe783fd2c5
Chad Woolley <thewoolleyman@gmail.com>
2018-04-26 13:04:56 -0700

    add some comments

--- nonapp/cicd/bk/pipeline.sh
+++ nonapp/cicd/bk/pipeline.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 set -eu
 
+# generate the pipeline.yml for the monorepo
 pipeline_file=/tmp/monorepo-pipeline.yml
 $(dirname "${BASH_SOURCE}")/generate-pipeline-yml ${pipeline_file}
 
+# upload the pipeline.yml, if it was generated (if any apps' builds need to be triggered)
 if [[ -e ${pipeline_file} ]]
 then
   cat ${pipeline_file} | buildkite-agent pipeline upload
 else
   echo "${pipeline_file} was not generated, not triggering any app builds."
 fi
-
